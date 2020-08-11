8b86ccab511b9e90aca63cd2f1ba7f985fabaaf1
Bearinawolfpack <bearinawolfpack@gmail.com>
2020-08-11 10:30:58 -0400

    Third Deliverable Done

--- src/App.js
+++ src/App.js
@@ -4,41 +4,66 @@ import PoemsContainer from "./PoemsContainer";
 import NewPoemForm from "./NewPoemForm";
 
 class App extends React.Component {
-  state = {
-    stateArray: [],
-    poemForm: false
-  }
-  componentDidMount(){
-    fetch('http://localhost:6001/poems')
-      .then(resp => resp.json())
-      .then(poems => this.setState({stateArray: poems}))
-      // .then(poems => console.log(poems))
-  }
+	state = {
+		stateArray: [],
+		poemForm: false,
+	};
+	componentDidMount() {
+		fetch("http://localhost:6001/poems")
+			.then((resp) => resp.json())
+			.then((poems) => this.setState({ stateArray: poems }));
+		// .then(poems => console.log(poems))
+	}
 
-  formClickHandler = () => {
-    // console.log(event)
-    if(this.state.poemForm){
-      this.setState({poemForm: false})
-    } else {
-      this.setState({poemForm: true})
-    }
-  }
-  
-  
+	formClickHandler = () => {
+		// console.log(event)
+		if (this.state.poemForm) {
+			this.setState({ poemForm: false });
+		} else {
+			this.setState({ poemForm: true });
+		}
+	};
 
-  render() {
-    // console.log(this.state)
-    // console.log(this.state.poemForm)
-    return (
-      <div className="app">
-        <div className="sidebar">
-          <button onClick={this.formClickHandler}>Show/hide new poem form</button>
-          {(this.state.poemForm) && <NewPoemForm />}
-        </div>
-        <PoemsContainer stateArray={this.state.stateArray}/>
-      </div>
-    );
-  }
+	submitHandler = (obj) => {
+		// console.log(obj)
+		let submitArray = [obj, ...this.state.stateArray];
+		// console.log(submitArray)
+    this.setState({ stateArray: submitArray });
+    
+    fetch("http://localhost:6001/poems", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({
+        'title': `${obj.title}`,
+        'author': `${obj.author}`,
+        'content': `${obj.content}`
+      })
+    })
+    .then(resp => resp.json())
+    .then(poem => console.log(poem))
+    
+	};
+
+	render() {
+		// console.log(this.state)
+		// console.log(this.state.poemForm)
+		return (
+			<div className="app">
+				<div className="sidebar">
+					<button onClick={this.formClickHandler}>
+						Show/hide new poem form
+					</button>
+					{this.state.poemForm && (
+						<NewPoemForm submitHandler={this.submitHandler} />
+					)}
+				</div>
+				<PoemsContainer stateArray={this.state.stateArray} />
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/NewPoemForm.js
+++ src/NewPoemForm.js
@@ -1,16 +1,50 @@
 import React from "react";
 
 class NewPoemForm extends React.Component {
-  render() {
-    return (
-      <form className="new-poem-form">
-        <input placeholder="Title" />
-        <input placeholder="Author" />
-        <textarea placeholder="Write your masterpiece here..." rows={10} />
-        <input type="submit" value="Share your masterpiece" />
-      </form>
-    );
-  }
+	state = {
+		title: "",
+    author: "",
+    content: "",
+	};
+
+	submitHandler = (event) => {
+		event.preventDefault();
+		this.props.submitHandler(this.state);
+	};
+
+	changeHandler = (event) => {
+		// console.log(event.target.value)
+		// console.log(event.target.name)
+		this.setState({ [event.target.name]: event.target.value });
+	};
+
+	render() {
+		// console.log(this.props.submitHandler)
+		return (
+			<form className="new-poem-form" onSubmit={this.submitHandler}>
+				<input
+					placeholder="Title"
+					name="title"
+					value={this.state.title}
+					onChange={this.changeHandler}
+				/>
+				<input
+					placeholder="Author"
+					name="author"
+					value={this.state.author}
+					onChange={this.changeHandler}
+				/>
+				<textarea
+          placeholder="Write your masterpiece here..."
+          name="content"
+          value={this.state.content}
+          onChange={this.changeHandler}
+					rows={10}
+				/>
+				<input type="submit" value="Share your masterpiece" />
+			</form>
+		);
+	}
 }
 
 export default NewPoemForm;

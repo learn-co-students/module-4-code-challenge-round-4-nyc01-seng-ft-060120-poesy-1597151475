b5c11b88d774cdac44e306ab9a7cb6dab40770a4
mhdhassan99 <mhassan.work99@gmail.com>
2020-08-11 10:28:23 -0400

    core deliverable are done

--- src/App.js
+++ src/App.js
@@ -2,16 +2,57 @@ import React from "react";
 import "./App.css";
 import PoemsContainer from "./PoemsContainer";
 import NewPoemForm from "./NewPoemForm";
+let baseUrl = 'http://localhost:6001/poems/'
 
 class App extends React.Component {
+
+  state = {
+    display: true,
+    poems: []
+  }
+
+  componentDidMount() {
+    fetch(baseUrl)
+    .then(response => response.json())
+    .then(poems => this.setState({poems: poems}))
+  }
+  showHandler = (e) => {
+   let newDisplay = !this.state.display
+   this.setState({
+     display: newDisplay
+   })
+  }
+
+  submitHandler = (obj) => {
+    fetch(baseUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({
+        title: obj.title,
+        content: obj.content,
+        author: obj.author
+      })
+    })
+    .then(response => response.json())
+    .then(newObj => {
+      let newPoems = [...this.state.poems, newObj]
+      this.setState({
+        poems: newPoems
+      })
+    })
+  }
+
   render() {
     return (
       <div className="app">
         <div className="sidebar">
-          <button>Show/hide new poem form</button>
-          {false && <NewPoemForm />}
+          <button onClick={this.showHandler}> Show/hide new poem form</button>
+          {this.state.display ? <NewPoemForm submitHandler={this.submitHandler}/> : null}
         </div>
-        <PoemsContainer />
+        <PoemsContainer poems={this.state.poems}/>
       </div>
     );
   }
--- src/Poem.js
+++ src/Poem.js
@@ -1,15 +1,21 @@
 import React from "react";
 
 class Poem extends React.Component {
+
+  markHandler = (e) => { 
+    e.target.innerHTML = "Mark as unread"
+  }
+  
   render() {
+    console.log(this.props)
     return (
       <div>
-        <h3>Title</h3>
-        <p>Content</p>
+        <h3> {this.props.poem.title}</h3>
+        <p>{this.props.poem.content}</p>
         <p>
-          <strong>- By Author</strong>
+          <strong>{this.props.poem.author}</strong>
         </p>
-        <button>Mark as read</button>
+        <button onClick={this.markHandler} >Mark as read</button>
       </div>
     );
   }

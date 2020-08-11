6772db8bf5679f80da40a348ece5829545e1e5af
Bearinawolfpack <bearinawolfpack@gmail.com>
2020-08-11 10:05:19 -0400

    Second Deliverable Done

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import NewPoemForm from "./NewPoemForm";
 
 class App extends React.Component {
   state = {
-    stateArray: []
+    stateArray: [],
+    poemForm: false
   }
   componentDidMount(){
     fetch('http://localhost:6001/poems')
@@ -14,19 +15,25 @@ class App extends React.Component {
       // .then(poems => console.log(poems))
   }
 
-  formClickHandler = (event) => {
-    console.log(event.target)
+  formClickHandler = () => {
+    // console.log(event)
+    if(this.state.poemForm){
+      this.setState({poemForm: false})
+    } else {
+      this.setState({poemForm: true})
+    }
   }
   
   
 
   render() {
     // console.log(this.state)
+    // console.log(this.state.poemForm)
     return (
       <div className="app">
         <div className="sidebar">
           <button onClick={this.formClickHandler}>Show/hide new poem form</button>
-          {false && <NewPoemForm />}
+          {(this.state.poemForm) && <NewPoemForm />}
         </div>
         <PoemsContainer stateArray={this.state.stateArray}/>
       </div>
